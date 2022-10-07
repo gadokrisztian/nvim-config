91888aa5f9e27cf9af5e30b047d40ce926c3f1de
gadokrisztian <gado.krisztian@montana.hu>
2022-10-07 11:46:16 +0200

    k

--- init.lua
+++ init.lua
@@ -6,7 +6,6 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
 	vim.api.nvim_command("packadd packer.nvim")
 	require("config.packer")
 	require("packer").sync()
-	return
 end
 
 require("config.preferences")
@@ -16,24 +15,24 @@ require("impatient")
 
 
 
-local generic_augroup = vim.api.nvim_create_augroup("config_generic", { clear = true })
-vim.api.nvim_create_autocmd("BufEnter", {
-	group = generic_augroup,
-	callback = function()
-		if vim.o.buftype ~= "terminal" then
-			vim.cmd("lcd %:p:h")
-		end
-	end
-})
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-	group = generic_augroup,
-	callback = function()
-		vim.highlight.on_yank({higroup = "IncSearch",
-							   timeout = 150,
-							   on_visual = true})
-	end
-})
+-- local generic_augroup = vim.api.nvim_create_augroup("config_generic", { clear = true })
+-- vim.api.nvim_create_autocmd("BufEnter", {
+-- 	group = generic_augroup,
+-- 	callback = function()
+-- 		if vim.o.buftype ~= "terminal" then
+-- 			vim.cmd("lcd %:p:h")
+-- 		end
+-- 	end
+-- })
+--
+-- vim.api.nvim_create_autocmd("TextYankPost", {
+-- 	group = generic_augroup,
+-- 	callback = function()
+-- 		vim.highlight.on_yank({higroup = "IncSearch",
+-- 							   timeout = 150,
+-- 							   on_visual = true})
+-- 	end
+-- })
 
 -- Disable some built-in plugins
 local disabled_built_ins = {
