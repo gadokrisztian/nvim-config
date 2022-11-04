443b588371d28b2e3537ecc5e4121fe1e9ed0a0b
gadokrisztian <gado.krisztian@montana.hu>
2022-11-04 09:50:33 +0100

    toggleterm update mapping

--- lua/user/toggleterm.lua
+++ lua/user/toggleterm.lua
@@ -30,10 +30,10 @@ function _G.set_terminal_keymaps()
   local opts = {noremap = true}
   vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
   vim.api.nvim_buf_set_keymap(0, 't', 'jk', [[<C-\><C-n>]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<C-\><C-n><C-W>h]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<C-\><C-n><C-W>j]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<C-\><C-n><C-W>k]], opts)
-  vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<C-\><C-n><C-W>l]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.api.nvim_buf_set_keymap(0, 't', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
 end
 
 vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
@@ -49,28 +49,11 @@ local python = Terminal:new({
     auto_scroll = true,
 })
 
+local node = Terminal:new({ cmd = "node", hidden = true })
+
 function _LAZYGIT_TOGGLE()
 	lazygit:toggle()
 end
-
-local node = Terminal:new({ cmd = "node", hidden = true })
-
-function _NODE_TOGGLE()
-	node:toggle()
-end
-
-local ncdu = Terminal:new({ cmd = "ncdu", hidden = true })
-
-function _NCDU_TOGGLE()
-	ncdu:toggle()
-end
-
-local htop = Terminal:new({ cmd = "htop", hidden = true })
-
-function _HTOP_TOGGLE()
-	htop:toggle()
-end
-
 
 function _IPYTHON_TOGGLE()
 	ipython:toggle()
@@ -85,6 +68,11 @@ local wk = require("which-key")
 
 wk.register({
     ["<leader>"] = {
-        p = { _PYTHON_TOGGLE, "Python" },
+        r = {
+            name='Run',
+            p={ _PYTHON_TOGGLE, "Python" },
+            l={ _LAZYGIT_TOGGLE, "Lazygit" },
+            i={ _IPYTHON_TOGGLE, "Ipython" },
+        },
     },
 })
